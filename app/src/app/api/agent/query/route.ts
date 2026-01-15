4c4dc4507f1967dec3a3031bc3e87d38ad85b3f6
kakizawaayato <chibairodori25@gmail.com>
2026-01-16 07:05:52 +0900

    WIP: TSVファイルをAIに読み込ませる暫定処理追加

--- app/src/app/api/agent/query/route.ts
+++ app/src/app/api/agent/query/route.ts
@@ -1,6 +1,8 @@
 // app/api/agent/query/route.ts
 export const runtime = "nodejs";
 
+import { promises as fs } from "node:fs";
+
 import { fpInstructor } from "@/agents/fpInstructor";
 import { jsonEditor } from "@/agents/jsonEditor";
 import { createSessionId } from "@/libs/google/createSessionId";
@@ -28,21 +30,23 @@ export async function POST(req: Request) {
 
   // === FP AI によるJson更新指示の生成 ===
   // 指示を作成 TODO プロンプト整備＆別ファイルへの切り出し
-  const requestToAi = `あなたの指示をインプットとして、この後、JSON更新AIがJSONを生成します。
+  const promptInstructToJsonEditor = `あなたの指示をインプットとして、この後、JSON更新AIがJSONを生成します。
     あなたはユーザーへの質問返しはせず、Json更新AIに対する指示を出してください。
     Json更新AIへの指示はJSON形式ではなく口語的にお願いしたいのと、JSON生成AIに対してJSONを生成するように明示的に指示してください。
     ユーザーのコメントは次のとおりです。`;
 
-  const fpAiStart = Date.now();
+  const fpAiInstructionStart = Date.now();
   const instructions = await fpInstructor(
     accessToken,
     userId,
     sessionId,
-    requestToAi,
+    promptInstructToJsonEditor,
     userMessage,
   );
   console.log("==============================");
-  console.log(`FP AI の処理結果（process time: ${Date.now() - fpAiStart}）`);
+  console.log(
+    `FP AI （Json更新指示）の処理結果（process time: ${Date.now() - fpAiInstructionStart}）`,
+  );
   console.log("-----");
   console.log(instructions);
   console.log("==============================");
@@ -56,6 +60,45 @@ export async function POST(req: Request) {
   );
   console.log("-----");
   console.log(response);
+  console.log("==============================");
+
+  // === ライフプラン表の作成 ===
+
+  // TODO input: ヒアリングJSON, output: ライフプランJSON
+  // TODO input: ライフプランJSON, output: ライフプランTSV
+
+  // === FP AI によるライフプランの生成 ===
+
+  const tsvPath = "dst/lifeplan_normalized_long.tsv";
+  const tsv = await fs.readFile(tsvPath, "utf-8");
+
+  //
+  const promptRequestFPCommentToLifePlan = [
+    "以下にライフプラン表を表すTSVデータがあります。",
+    "TSVはタブ区切りで、1行目はヘッダーです。",
+    "ライフプラン表とヒアリング内容を鑑みて、FPとしての評価及びユーザーが行うべきネクストアクションを提示してください。",
+    "",
+    "```tsv",
+    tsv.trimEnd(), // 末尾改行の暴発防止
+    "```",
+  ].join("\n");
+
+  console.log(promptRequestFPCommentToLifePlan);
+
+  const fpAiCommentStart = Date.now();
+  const comment = await fpInstructor(
+    accessToken,
+    userId,
+    sessionId,
+    promptRequestFPCommentToLifePlan,
+    userMessage,
+  );
+  console.log("==============================");
+  console.log(
+    `FP AI (コメント)の処理結果（process time: ${Date.now() - fpAiCommentStart}）`,
+  );
+  console.log("-----");
+  console.log(comment);
   console.log("==============================");
 
   // === レスポンス ===
