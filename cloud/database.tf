968a5646a499b8a588d4f2f811c011a4e929ac33
kakizawaayato <chibairodori25@gmail.com>
2026-01-23 09:09:09 +0900

    cloud: インスタンスの中にDB及びユーザーの作成（PWはCI/CDの中で渡す）

--- cloud/database.tf
+++ cloud/database.tf
@@ -12,11 +12,24 @@ resource "google_sql_database_instance" "main_db" {
   name             = "life-compass-postgres"
   region           = "asia-northeast1"
   database_version = "POSTGRES_15"
-  
+
   settings {
     tier = "db-f1-micro" # 最小構成
   }
 
   deletion_protection = true
   depends_on          = [google_project_service.required_apis]
+}
+
+# Cloud SQL データベース
+resource "google_sql_database" "app_db" {
+  name     = "life_compass"
+  instance = google_sql_database_instance.main_db.name
+}
+
+# Cloud SQL ユーザー
+resource "google_sql_user" "app_user" {
+  name     = "app_user"
+  instance = google_sql_database_instance.main_db.name
+  password = var.db_password
 }
